43a32fa8963e26c57b5eebadf613b2204ef15d95
nsh2207 <sn4165@nyu.edu>
2025-01-02 16:48:16 -0500

    Implemented stacks in  Exercise_2

--- Exercise_2.cpp
+++ Exercise_2.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h> 
+#include <iostream>
 using namespace std; 
   
 // A structure to represent a stack 
@@ -19,21 +20,37 @@ StackNode* newNode(int data)
 int isEmpty(StackNode* root) 
 { 
     //Your code here 
+    return root==NULL;
 } 
   
 void push(StackNode** root, int data) 
 { 
     //Your code here 
+    StackNode* node = newNode(data);
+    node->next=*root;
+    *root=node;
 } 
   
 int pop(StackNode** root) 
 { 
     //Your code here 
+    if(isEmpty(*root)){
+        cout<<"Stack is empty";
+        return 0;
+    }
+    int val = (*root)->data;
+    *root=(*root)->next;
+    return val;
 } 
   
 int peek(StackNode* root) 
 { 
     //Your code here 
+    if(isEmpty(root)){
+        cout<<"Stack is empty";
+        return 0;
+    }
+    return root->data;
 } 
   
 int main() 
